d608d04c3799604e9bd5486d724a3e9267439d1b
David Ng <dave@codeaurora.org>
2012-08-10 20:36:59 -0700

    msm8974: Prevent device from sleeping during inactivity
    Disable device sleep/suspend for initial bring-up.
    
    Change-Id: I82f4904509fc4a66b21b53c70c3e23eec3b9dbb0

--- msm8974.mk
+++ msm8974.mk
@@ -3,5 +3,7 @@ $(call inherit-product, device/qcom/common/common.mk)
 PRODUCT_NAME := msm8974
 PRODUCT_DEVICE := msm8974
 
+DEVICE_PACKAGE_OVERLAYS := device/qcom/msm8974/overlay
+
 #fstab.msm8974
 PRODUCT_PACKAGES += fstab.msm8974
