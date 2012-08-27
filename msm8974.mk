8b9378360b74b33ec77d847c29c7b93adc9e9214
Bhakthavatsala Raghavendra <braghave@codeaurora.org>
2012-08-27 14:28:12 -0700

    Bluetooth: Add Bluetooth configuration files for msm8974
    Adds the Bluetooth related configuration files to package as
    part of system image
    
    Change-Id: Iabeab88f4193ff57421be80bb305c560db3f3a7e

--- msm8974.mk
+++ msm8974.mk
@@ -5,5 +5,9 @@ PRODUCT_DEVICE := msm8974
 
 DEVICE_PACKAGE_OVERLAYS := device/qcom/msm8974/overlay
 
+# Bluetooth configuration files
+PRODUCT_COPY_FILES += \
+    system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf \
+
 #fstab.qcom
 PRODUCT_PACKAGES += fstab.qcom
