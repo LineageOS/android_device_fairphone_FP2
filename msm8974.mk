7a01279573151664d4f6c2bcca6731d7d8068b9e
Phani Kumar Uppalapati <phanik@codeaurora.org>
2012-12-08 19:02:18 -0800

    audio: Add UCM files for B-family
    - Add B-family UCM files under device/qcom/msm8974/
    
    Change-Id: I3de379853446f4db6d79fbc3d8108ce7f737ad18

--- msm8974.mk
+++ msm8974.mk
@@ -7,6 +7,13 @@ PRODUCT_DEVICE := msm8974
 PRODUCT_COPY_FILES += \
     device/qcom/msm8974/audio_policy.conf:system/etc/audio_policy.conf \
 
+# audio UCM files
+PRODUCT_COPY_FILES += device/qcom/msm8974/snd_soc_msm/snd_soc_msm_Taiko:system/etc/snd_soc_msm/snd_soc_msm_Taiko \
+                      device/qcom/msm8974/snd_soc_msm/snd_soc_msm_Taiko_CDP:system/etc/snd_soc_msm/snd_soc_msm_Taiko_CDP \
+                      device/qcom/msm8974/snd_soc_msm/snd_soc_msm_Taiko_Fluid:system/etc/snd_soc_msm/snd_soc_msm_Taiko_Fluid \
+                      device/qcom/msm8974/snd_soc_msm/snd_soc_msm_Taiko_liquid:system/etc/snd_soc_msm/snd_soc_msm_Taiko_liquid \
+
+
 # Bluetooth configuration files
 PRODUCT_COPY_FILES += \
     system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf \
