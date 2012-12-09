c2fc1c4d4c43064fb052eaf24b475cf53d83cf1b
Harmandeep Singh <hsingh@codeaurora.org>
2012-12-08 18:16:27 -0800

    audio: add audio_policy.conf file
    - Add audio policy.conf file for 8974 target
    
    Change-Id: Ic37bfc6f3dfc941d7a5a2bf7b0e85c69aad45bf2

--- msm8974.mk
+++ msm8974.mk
@@ -3,6 +3,10 @@ $(call inherit-product, device/qcom/common/common.mk)
 PRODUCT_NAME := msm8974
 PRODUCT_DEVICE := msm8974
 
+# Audio configuration file
+PRODUCT_COPY_FILES += \
+    device/qcom/msm8974/audio_policy.conf:system/etc/audio_policy.conf \
+
 # Bluetooth configuration files
 PRODUCT_COPY_FILES += \
     system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf \
