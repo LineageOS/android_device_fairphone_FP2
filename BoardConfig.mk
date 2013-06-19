93c69cfa81831be74b6822607093fb91637ffea5
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2013-06-19 14:18:00 -0600

    msm8974: BoardConfig: Add board specific SELinux policy variables
    These variables are used by Build System to generate policy files.
    
    Change-Id: I1ddf5f95cba670449fb21c8baab2d0f1fb1ea29e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,3 +94,10 @@ TARGET_USES_NEW_ION_API :=true
 
 TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_msm
+
+# Board specific SELinux policy variable definitions
+BOARD_SEPOLICY_DIRS := \
+       device/qcom/common/sepolicy
+
+BOARD_SEPOLICY_UNION := \
+       netd.te
