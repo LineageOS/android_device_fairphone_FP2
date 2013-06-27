aa5c26eaf0d9465773bd9a80f41daa54ed77ce5b
vivek mehta <mvivek@codeaurora.org>
2013-06-27 14:26:26 -0700

    msm8974: Add QC BSP amd AV Enhancement flag in Product file
    - Add QC BSP flag and AV enhancement flag in Product file and
      remove them from Board config file
    - This flag is set in Board Config file and used in Product
      config file. But Product file is called before Board file.
      so when this check is made in Product config file this will
      always be false.
    
    Change-Id: Ia801825ac53f711a48900797d95bcb70f14b37e3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,9 +86,6 @@ TARGET_KRAIT_BIONIC_PLDSIZE := 64
 
 TARGET_USES_ION := true
 TARGET_USES_NEW_ION_API :=true
-TARGET_USES_QCOM_BSP := true
 
 TARGET_ADDITIONAL_BOOTCLASSPATH := qcmediaplayer:WfdCommon:oem-services:qcom.fmradio
 
-# Add QC Video Enhancements flag
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
