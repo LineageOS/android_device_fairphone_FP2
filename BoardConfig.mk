8aa28f0f8e35415bdb4f081d509f7e51c016fb4a
Ravi Kumar Siddojigari <rsiddoji@codeaurora.org>
2014-10-07 14:29:53 +0530

    msm8974: Removed "BOARD_SEPOLICY_UNION" and "BOARD_SEPOLICY_DIRS" as we are moving all  sepolicy to device/qcom/sepolicy folder .
    Change-Id: I717804242d8c65ceba4ef8c0331d0f1a54d036e3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,10 +106,3 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_msm
 
-# Board specific SELinux policy variable definitions
-BOARD_SEPOLICY_DIRS := \
-       device/qcom/common/sepolicy
-
-BOARD_SEPOLICY_UNION := \
-       netd.te
-
