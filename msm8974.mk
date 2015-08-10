1ebdcd1136af40def0c08079fb7e7d24b424bcea
Susheel Yadagiri <syadagir@codeaurora.org>
2015-08-10 16:51:45 -0700

    Add dpmapi to PRODUCT_BOOT_JARS.
    Add dpmapi jar used by dpmframework jar to
    PRODUCT_BOOT_JARS.
    
    Change-Id: Ic3a23b7dfcf127e90dd8ef5aebc6606218689950

--- msm8974.mk
+++ msm8974.mk
@@ -122,12 +122,13 @@ endif
 
 PRODUCT_BOOT_JARS += tcmiface
 PRODUCT_BOOT_JARS += qcmediaplayer
-PRODUCT_BOOT_JARS += com.qti.dpmframework
 
-#ifneq ($(strip $(QCPATH)),)
+ifneq ($(strip $(QCPATH)),)
 PRODUCT_BOOT_JARS += WfdCommon
 PRODUCT_BOOT_JARS += qcom.fmradio
 #PRODUCT_BOOT_JARS += security-bridge
 #PRODUCT_BOOT_JARS += qsb-port
 #PRODUCT_BOOT_JARS += oem-services
-#endif
+PRODUCT_BOOT_JARS += com.qti.dpmframework
+PRODUCT_BOOT_JARS += dpmapi
+endif
