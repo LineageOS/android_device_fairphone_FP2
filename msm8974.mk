d77a34503af16acf94b99b299be34eefd5c80676
Paras Nagda <pnagda@codeaurora.org>
2015-09-16 15:34:22 +0530

    msm8974: Add support for 'frame-rate' key
    Create new media_codecs_performace.xml to add
    frame-rate key support required for video perf
    cts tests.
    
    Change-Id: Ia42283596c986539f8ce7b7744932aeacad7aadd

--- msm8974.mk
+++ msm8974.mk
@@ -12,7 +12,8 @@ endif #TARGET_USES_QCOM_BSP
 # media_profiles and media_codecs xmls for 8974
 ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
 PRODUCT_COPY_FILES += device/qcom/msm8974/media/media_profiles_8974.xml:system/etc/media_profiles.xml \
-                      device/qcom/msm8974/media/media_codecs_8974.xml:system/etc/media_codecs.xml
+                      device/qcom/msm8974/media/media_codecs_8974.xml:system/etc/media_codecs.xml \
+                      device/qcom/msm8974/media/media_codecs_performance_8974.xml:system/etc/media_codecs_performance.xml
 endif  #TARGET_ENABLE_QC_AV_ENHANCEMENTS
 
 $(call inherit-product, device/qcom/common/common.mk)
