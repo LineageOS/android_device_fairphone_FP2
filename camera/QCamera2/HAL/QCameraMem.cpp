323de6ad4957f8ce690ba38947f36fded3a8e1ba
Santhosh Kumar Thimmanna Bhattar <sthim@codeaurora.org>
2016-10-07 18:49:42 +0530

    FP2: QCamera2: Update metadata index with media extension flag
    Issue:
    Video recording stop is failing for native handle buffer type
    due to meta data buffer index is getting updated incorrectly.
    
    Fix:
    Update meta data buffer index properly with media extension flag
    
    Change-Id: Ie8c834ac8add5fe8627e78eed2654d4f98cff9b3

--- camera/QCamera2/HAL/QCameraMem.cpp
+++ camera/QCamera2/HAL/QCameraMem.cpp
@@ -1414,6 +1414,7 @@ int QCameraVideoMemory::getMatchBufIndex(const void *opaque,
 {
     int index = -1;
     if (metadata) {
+#ifdef USE_MEDIA_EXTENSIONS
         const media_metadata_buffer *packet =
             (const media_metadata_buffer *)opaque;
         native_handle_t *nh = NULL;
@@ -1430,12 +1431,14 @@ int QCameraVideoMemory::getMatchBufIndex(const void *opaque,
                 }
             }
         }
+#else
         for (int i = 0; i < mMetaBufCount; i++) {
             if (mMetadata[i]->data == opaque) {
                 index = i;
                 break;
             }
         }
+#endif
     } else {
         for (int i = 0; i < mBufferCount; i++) {
             if (mCameraMemory[i]->data == opaque) {
