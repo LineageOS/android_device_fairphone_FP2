ce24f34fb14e98ffd1423da1944a3b8365422360
Diego Wilson <diegowilson@google.com>
2017-02-24 17:07:28 -0800

    FP2: GPS: Move device dependent modules to /vendor
    Change-Id: I8229a6e4a9512a03cbc6c9522bb1dc0ce988d1c9

--- gps/loc_api/libloc_api_50001/Android.mk
+++ gps/loc_api/libloc_api_50001/Android.mk
@@ -6,6 +6,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libloc_eng
 LOCAL_MODULE_OWNER := qcom
+LOCAL_VENDOR_MODULE := true
 
 LOCAL_MODULE_TAGS := optional
 
@@ -62,6 +63,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := gps.msm8974
 LOCAL_MODULE_OWNER := qcom
+LOCAL_VENDOR_MODULE := true
 
 LOCAL_MODULE_TAGS := optional
 
