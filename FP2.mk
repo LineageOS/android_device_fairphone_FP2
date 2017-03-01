fced8cf0c5291cdb0bbca71228d12302ac6a4d24
Michael Bestas <mikeioannina@gmail.com>
2017-03-01 19:09:30 +0200

    FP2: QCamera2: Build camera HAL from source
    * Remove legacy HAL1 hacks, prebuilt libraries and camera shims
    * Use non-legacy HIDL camera implementations
    
    Change-Id: I2f8e13d266d7c1615648d75c1c0da299ee59551a

--- FP2.mk
+++ FP2.mk
@@ -235,7 +235,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl \
+    camera.msm8974 \
     Snap
 
 # Seccomp
