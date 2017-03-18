5d0ef582428faebf730dc9702c20cb0a5ed6d0d3
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-03-19 01:01:26 +0800

    FP2: QCamera2: Wait for sensorservice initialization before opening camera
    Waits for sensorservice initialization before opening the actual HAL.
    
    Moved to OSS HAL from CameraWrapper. Originally by Danny Baumann <dannybaumann@web.de>.
    
    Change-Id: I876d2f0a57e17da392d451987047189b05a5e72b

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -49,7 +49,7 @@ LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
-LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl
+LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl libgui
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
