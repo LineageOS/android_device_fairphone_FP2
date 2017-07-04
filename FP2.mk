b38fd8a38cf1d77bcf6573fef179e762c1e8c9ea
chrmhoffmann <chrmhoffmann@gmail.com>
2017-07-04 21:30:08 +0200

    FP2: aapt display sizes

--- FP2.mk
+++ FP2.mk
@@ -94,6 +94,10 @@ PRODUCT_PACKAGES += \
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
+# Display
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Graphics
 PRODUCT_PACKAGES += \
     copybit.msm8974 \
