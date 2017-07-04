4720103e3ef0fa877c5affda8b2bdb1d50d89d0c
chrmhoffmann <chrmhoffmann@gmail.com>
2017-07-04 21:32:01 +0200

    FP2: ant

--- FP2.mk
+++ FP2.mk
@@ -108,6 +108,7 @@ PRODUCT_PACKAGES += \
 
 # Features
 PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
@@ -168,12 +169,12 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf \
     wpa_supplicant_overlay.conf
 
-#ANT stack
+# ANT stack
 PRODUCT_PACKAGES += \
-        AntHalService \
-        libantradio \
-        ANTRadioService \
-        antradio_app
+   ANTRadioService \
+   AntHalService \
+   antradio_app \
+   libantradio
 
 # GPS
 PRODUCT_PACKAGES += \
