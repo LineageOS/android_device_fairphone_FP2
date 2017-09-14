a45d47f9f819ea04699a2d1f93b266b8a5b471c8
Ethan Chen <intervigil@gmail.com>
2017-09-14 01:01:28 -0700

    FP2: Add seccomp policy
    Change-Id: I1d5017228a4a9318f8a77ef4036532d64718ecbb

--- FP2.mk
+++ FP2.mk
@@ -238,6 +238,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Snap
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
