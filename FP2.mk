3c7966078f969c28a01feed9768007ef23f0a949
Michael Bestas <mkbestas@lineageos.org>
2018-01-17 18:59:09 +0200

    FP2: Move sec_config to vendor
    Change-Id: Ie5f7c8f96243a4ed354f87d78ede9176a46b44be

--- FP2.mk
+++ FP2.mk
@@ -30,7 +30,7 @@ PRODUCT_COPY_FILES += \
 
 # IPC router
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
 # Audio
 PRODUCT_COPY_FILES += \
