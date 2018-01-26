3437ba26ad8894949e9d57feb79a9a6d9e8e760d
Bruno Martins <bgcngm@gmail.com>
2018-01-26 10:26:43 +0000

    FP2: Move input device config and keylayout files to vendor
    Change-Id: I1e540227ca30b4fbeb3789996af37ad7ddc60ae1

--- FP2.mk
+++ FP2.mk
@@ -176,7 +176,7 @@ PRODUCT_COPY_FILES += \
 
 # Input device files
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
+    $(LOCAL_PATH)/configs/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
 
 # Wifi
 PRODUCT_COPY_FILES += \
