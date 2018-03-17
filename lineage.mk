48f3692d1d23615638e34f2151e152e14e264702
chrmhoffmann <chrmhoffmann@gmail.com>
2018-03-17 09:58:58 +0100

    FP2: Rebrand vendor cm to lineage
    Change-Id: Ifae584a3cededbd89294d6b79ad9ef29e763b65d

--- lineage.mk
+++ lineage.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,8 +18,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from FP2 device
 $(call inherit-product, device/fairphone/FP2/FP2.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_FP2
 PRODUCT_DEVICE := FP2
