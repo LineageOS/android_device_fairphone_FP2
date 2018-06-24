a064893a45b6630466161abcd7050c2aa57a42cd
chrmhoffmann <chrmhoffmann@gmail.com>
2018-06-24 08:35:16 +0200

    FP2: build vibrator HAL
    Change-Id: I6cdad937a8d856f77da1c4aa89daea288d082218

--- FP2.mk
+++ FP2.mk
@@ -246,6 +246,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
 # Proximity Calibration
 PRODUCT_PACKAGES += \
     ProximitySensorTools
