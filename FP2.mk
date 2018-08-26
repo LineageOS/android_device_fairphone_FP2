2d26b71cc9d640f2d6de60993421df6dd63e7220
jrior001 <jriordan001@gmail.com>
2018-08-26 18:47:33 -0400

    FP2: Use common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- FP2.mk
+++ FP2.mk
@@ -173,6 +173,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8974.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8974.conf
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # Input device files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
