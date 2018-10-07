a0010b6085b97775c9a9b09ea0c5225cdccb3e25
Bruno Martins <bgcngm@gmail.com>
2018-10-08 00:25:18 +0100

    FP2: Create root folders outside of vendor init script
     * The core SELinux policies for vendor_init are being restricted as to
       comply with Treble. Simply adding a local rule to allow creating
       folders under rootfs would be enough, but at the same time defining
       the extra root folders and making use of AID/fs configuration file
       to set perms is more neat and clean. This seems not to be required
       when the partitions are mounted directly under rootfs, however it's
       not the case here because radio and adsp partitions are respectively
       mounted under /firmware/radio and /firmware/adsp and therefore
       /firmware must exist beforehand. While at it, drop the creation
       of persist root folder as there's no evidence of HTC MSM8974-based
       devices even having such partition.
    
    Change-Id: Idabd7ae72e4c4fd9daac5ba3be3e6236f79f251b

--- config.fs
+++ config.fs
@@ -6,3 +6,15 @@ value:2951
 
 [AID_VENDOR_RFS_SHARED]
 value:2952
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
