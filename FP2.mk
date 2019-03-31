7c194ca9c7b793c5e58b5fba3dfd28e02bddee97
chrmhoffmann <chrmhoffmann@gmail.com>
2019-03-31 17:25:42 +0200

    Revert "FP2: Default OMX service to non-Treble"
    This reverts commit 100f6434c3c0057240eccf4bbbd291c3951d64d3.
    
    Change-Id: I2b650a77df602f38eea1ad59749cfa0d3e84ac6f

--- FP2.mk
+++ FP2.mk
@@ -48,9 +48,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/mixer_paths_auxpcm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_auxpcm.xml
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.media.treble_omx=false
-
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.primary.msm8974 \
